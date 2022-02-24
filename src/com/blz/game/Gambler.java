e7ea71b336d74228305364c125f392b3cff52a07
Rehan Shekh <rehanrshekh@gmail.com>
2022-02-24 15:07:13 +0530

    UC_4

--- src/com/blz/game/Gambler.java
+++ src/com/blz/game/Gambler.java
@@ -7,7 +7,7 @@ public class Gambler {
     public static void main(String[] args) {
         double startamount = 100;
         int day;
-        for (day = 1; day <= 30; day++) {
+        for (day = 1; day <= 20; day++) {
             int stake = 100;
             while (stake > (0.5 * AMOUNT) && stake < (1.5 * AMOUNT)) {
                 int chance = (int) (Math.floor(Math.random() * 10) % 2);
@@ -20,14 +20,17 @@ public class Gambler {
             }
             {
                 if (stake > AMOUNT) {
-                    System.out.println("Day " + day + ":Won, Initial stake is $" + (startamount) + " and total is " + "$" + (startamount + (0.5 * AMOUNT)));
                     startamount = startamount + (0.5 * AMOUNT);
                 } else if (stake < AMOUNT) {
-                    System.out.println("Day " + day + ":Lost, Initial stake is $" + (startamount) + " and total is " + "$" + (startamount - (0.5 * AMOUNT)));
                     startamount = startamount - (0.5 * AMOUNT);
                 }
 
             }
         }
+        if (startamount > AMOUNT) {
+            System.out.println("Won, After 20 days, the total amount is:" + startamount);
+        } else if (startamount < AMOUNT) {
+            System.out.println("Lost, After 20 days, the total amount is:" + startamount);
+        } else System.out.println("Neither Won nor Lose, After 20 days, the total amount is:" + startamount);
     }
 }
