611b64f11084e252d45b3dc32ab4d57f14be2a0a
Rehan Shekh <rehanrshekh@gmail.com>
2022-02-24 19:51:19 +0530

    UC_4

--- src/com/blz/game/Gambler.java
+++ src/com/blz/game/Gambler.java
@@ -3,34 +3,37 @@ package com.blz.game;
 public class Gambler {
     public static final int BET = 1;
     public static final int AMOUNT = 100;
+    public static int WON = 0;
+    public static int LOSE = 0;
 
     public static void main(String[] args) {
-        double startamount = 100;
+        double amount = 0;
         int day;
-        for (day = 1; day <= 20; day++) {
+        for (day = 0; day < 20; day++) {
             int stake = 100;
             while (stake > (0.5 * AMOUNT) && stake < (1.5 * AMOUNT)) {
-                int chance = (int) (Math.floor(Math.random() * 10) % 2);
-                if (chance == BET) {
+                int bet = (int) (Math.floor(Math.random() * 10) % 2);
+                if (bet == BET) {
+                    WON++;
                     stake++;
-                } else if (chance == 0) {
+                } else if (bet == 0) {
+                    LOSE++;
                     stake--;
                 }
-
             }
-            {
-                if (stake > AMOUNT) {
-                    startamount = startamount + (0.5 * AMOUNT);
-                } else if (stake < AMOUNT) {
-                    startamount = startamount - (0.5 * AMOUNT);
-                }
-
+            if (stake > AMOUNT) {
+                WON++;
+                amount = amount + 50;
+            } else if (stake < AMOUNT) {
+                LOSE++;
+                amount = amount - 50;
             }
         }
-        if (startamount > AMOUNT) {
-            System.out.println("Won, After 20 days, the total amount is:" + startamount);
-        } else if (startamount < AMOUNT) {
-            System.out.println("Lost, After 20 days, the total amount is:" + startamount);
-        } else System.out.println("Neither Won nor Lose, After 20 days, the total amount is:" + startamount);
+        if (WON > LOSE) {
+            System.out.println("After Day " + day + ", the total amount won is $" + amount);
+        } else if (LOSE > WON) {
+            System.out.println("After Day " + day + ", the total amount lose is $" + amount);
+        } else System.out.println("Neither Won nor Lose");
     }
 }
+
